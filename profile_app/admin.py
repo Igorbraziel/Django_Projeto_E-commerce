fdc58b546895cf44043be4aab0b37e66fba4ad14
braziel <braziel@discente.ufg.br>
2025-01-24 15:07:48 -0300

    Creating the model UserProfile

--- profile_app/admin.py
+++ profile_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from profile_app.models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = 'id', 'user', 'age', 'birth_date', 'cpf',
+    list_display_links = 'id',
+    search_fields = 'id',
+    list_per_page = 10
+    ordering = '-id',
--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ValidationError
+from utils.validators import cpf_validator
+import re
 
-# Create your models here.
+class UserProfile(models.Model):
+    class Meta:
+        verbose_name = 'user profile'
+        verbose_name_plural = 'users profiles'
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField()
+    birth_date = models.DateField()
+    cpf = models.CharField(max_length=11)
+    address = models.CharField(max_length=50)
+    number = models.CharField(max_length=5)
+    complement = models.CharField(max_length=30)
+    neighborhood = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    city = models.CharField(max_length=30)
+    state = models.CharField(
+        default='SP', max_length=2,
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        ) 
+    )
+    
+    def clean(self):
+        error_messages = {}
+        if not cpf_validator(self.cpf):
+            error_messages['cpf'] = 'Invalid CPF'
+        if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+            error_messages['cep'] = 'Invalid CEP'
+        if error_messages:
+            raise ValidationError(error_messages)
+            
+        return super().clean()
+        
+    
+    def __str__(self):
+        if self.user.first_name and self.user.last_name:
+            return f'{self.user.first_name} {self.user.last_name}'
+        return self.user.username
